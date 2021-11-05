1488aa1895cfe199c5ba1e98e9104fe23c41cf90
Zuke27 <wstaylor27@gmail.com>
2021-11-04 21:56:09 -0400

    Update routes in controller

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,12 @@
 const router = require("express").Router();
+const userRoutes = require("./userRoutes");
+const expenseRoutes = require("./expenseRoutes");
+const categoryRoutes = require(".categoryRoutes");
+const budgetRoutes = require("../../controllers/api/budgetRoutes");
 
-const userRoutes = require("./user-routes");
-
-router.use("/users", userRoutes);
+router.use("/user", userRoutes);
+router.use("/expense", expenseRoutes);
+router.use("/category", categoryRoutes);
+router.use("/budget", budgetRoutes);
 
 module.exports = router;
