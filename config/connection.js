c7ce16f15ce7c0d397018afeb980e444342b8d5a
Jordan DuBose <jordan.dubose1@gmail.com>
2021-11-06 11:17:58 -0400

    updating connection file and login handlebars

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,13 @@
-const Sequelize = require("sequelize");
-require("dotenv").config();
+const Sequelize = require('sequelize');
+require('dotenv').config();
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: "localhost",
-    dialect: "mysql",
+    host: 'localhost',
+    dialect: 'mysql',
     port: 3306,
   }
 );
