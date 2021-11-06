c6b8859d6c568b33a17af3590865ad18e3a65091
Timrob584 <timrob584@gmail.com>
2021-11-06 08:50:20 -0400

    created navbar, sidebar, footer, and main overview handlebar

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,12 @@
-const router = require("express").Router();
+const router = require('express').Router();
+const path = require('path');
 
-const userRoutes = require("./user-routes");
+const userRoutes = require('./user-routes');
 
-router.use("/users", userRoutes);
+router.use('/users', userRoutes);
+
+router.get('/', async (req, res) => {
+  res.render('homepage');
+});
 
 module.exports = router;
