0fa6aca7c4bffcf32f650ca6cfd0f7283285f6c4
Zuke27 <wstaylor27@gmail.com>
2021-11-06 14:04:58 -0400

    Changes with Carl

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-// const apiRoutes = require('./api');
+const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
 
 router.use('/', homeRoutes);
