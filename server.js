b9ca185968fe2a0684c8d63ec93cff0907aa8cef
Zuke27 <wstaylor27@gmail.com>
2021-11-09 18:43:48 -0500

    Update index model

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const path = require('path');
+
 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
