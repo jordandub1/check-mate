757f5119e6c393fbc7ec8ff392fab50e90a26274
Jared Kohrt <jkohrt7@gmail.com>
2021-11-10 11:44:33 -0500

    Added transaction to index.js. Added Transaction CRUD operations.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,12 +3,13 @@ const userRoutes = require('./user-routes');
 const expenseRoutes = require('./expenseRoutes');
 const categoryRoutes = require('./categoryRoutes');
 const budgetRoutes = require('../../controllers/api/budgetRoutes');
+const transactionRoutes = require('./transactionRoutes')
 
 router.use('/user', userRoutes);
 router.use('/expense', expenseRoutes);
 router.use('/category', categoryRoutes);
-// router.use('/budget', budgetRoutes);
-
+//router.use('/budget', budgetRoutes);
+router.use('/transaction', transactionRoutes);
 router.use('/users', userRoutes);
 
 module.exports = router;
