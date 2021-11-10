7cd10f3b1c37aa98dd0f7fcbe4e6be0fe1acea58
Jordan DuBose <jordan.dubose1@gmail.com>
2021-11-10 16:09:44 -0500

    updating home-routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,28 +1,26 @@
 const router = require('express').Router();
-const { Project, User } = require('../models');
+const { Budget, User } = require('../models');
 const withAuth = require('../utils/auth');
 
-//TODO: replace with our own info
 // router.get('/user', async (req, res) => {
-//TODO: customize
 router.get('/user', async (req, res) => {
   try {
     // Get all projects and JOIN with user data
-    const projectData = await Project.findAll({
+    const budgetData = await Budget.findAll({
       include: [
         {
           model: User,
-          attributes: ['name'],
+          attributes: ['id'],
         },
       ],
     });
 
     // Serialize data so the template can read it
-    const projects = projectData.map((project) => project.get({ plain: true }));
+    const budgets = budgetData.map((budget) => budget.get({ plain: true }));
 
     // Pass serialized data and session flag into template
     res.render('homepage', {
-      projects,
+      budgets,
       logged_in: req.session.logged_in,
     });
   } catch (err) {
@@ -30,42 +28,22 @@ router.get('/user', async (req, res) => {
   }
 });
 
-router.get('/project/:id', async (req, res) => {
+router.get('/budget/:id', async (req, res) => {
   try {
-    const projectData = await Project.findByPk(req.params.id, {
+    const budgetData = await Budget.findByPk(req.params.id, {
       include: [
         {
           model: User,
-          attributes: ['name'],
+          attributes: ['id'],
         },
       ],
     });
 
-    const project = projectData.get({ plain: true });
+    const budget = budgetData.get({ plain: true });
 
-    res.render('project', {
-      ...project,
+    res.render('budget', {
+      ...budget,
       logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, res) => {
-  try {
-    // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: Project }],
-    });
-
-    const user = userData.get({ plain: true });
-
-    res.render('profile', {
-      ...user,
-      logged_in: true,
     });
   } catch (err) {
     res.status(500).json(err);
@@ -82,7 +60,7 @@ router.get('/', (req, res) => {
   res.render('login');
 });
 
-router.get('/overview', async (req, res) => {
+router.get('/overview', withAuth, async (req, res) => {
   res.render('homepage');
 });
 
