33c2f0d2557eaac42c56b9ab503bb480e6ce7e40
Jared Kohrt <jkohrt7@gmail.com>
2021-11-15 21:29:27 -0500

    Line graph card skeleton

--- public/js/logout.js
+++ public/js/logout.js
@@ -48,7 +48,6 @@ async function newIncomeHandler(event) {
   event.preventDefault();
   
   const user_id = (document.querySelector('#transaction-modal')).getAttribute("userInfo");
-  console.log(user_id)
   const tran_name = document.querySelector('#trx_name').value;
   const tran_date = document.querySelector('#trx_date').value;
   const amount = 2000;
