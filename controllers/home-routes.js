f3c08e41e53823fc74bc5efaf1fdb11597eafe4b
Jordan DuBose <jordan.dubose1@gmail.com>
2021-11-16 15:12:27 -0500

    updating UI and budget-display page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -112,7 +112,59 @@ router.get('/overview', withAuth, async (req, res) => {
 
 //Redirect to Budget Display Page
 router.get('/budget-display', async (req, res) => {
-  res.render('budget-display');
+  try {
+    //redirect to login page if user is not logged in
+    console.log(req.session.logged_in);
+    if (!req.session.logged_in) {
+      res.redirect('/');
+      return;
+    }
+
+    // Get all Transactions and JOIN with user data
+    const budgetData = await Budget.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['id'],
+          where: {
+            id: req.session.user_id, //only get Transactions that match user_id
+          },
+        },
+      ],
+    });
+
+    const transactionData = await Transaction.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['id'],
+          where: {
+            id: req.session.user_id, //only get Transactions that match user_id
+          },
+        },
+      ],
+    });
+
+    const transactions = transactionData.map((transaction) =>
+      transaction.get({ plain: true })
+    );
+
+    const userData = await User.findByPk(req.session.user_id);
+    const user = userData.get({ plain: true });
+
+    // Serialize data so the template can read it
+    const budgets = budgetData.map((budget) => budget.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('budget-display', {
+      budgets,
+      transactions,
+      user,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/transaction-display', async (req, res) => {
@@ -137,6 +189,9 @@ router.get('/transaction-display', async (req, res) => {
       ],
     });
 
+    const userData = await User.findByPk(req.session.user_id);
+    const user = userData.get({ plain: true });
+
     // Serialize data so the template can read it
     const transactions = transactionData.map((transaction) =>
       transaction.get({ plain: true })
@@ -145,6 +200,7 @@ router.get('/transaction-display', async (req, res) => {
     // Pass serialized data and session flag into template
     res.render('transaction-display', {
       transactions,
+      user,
       logged_in: req.session.logged_in,
     });
   } catch (err) {
